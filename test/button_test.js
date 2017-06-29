2fc2ab3e3b8fe7d987c7f96a545ee4a4380d3575
Marc Robert Wong <marcrobertwong@gmail.com>
2017-06-29 15:03:26 -0700

    Fixed linting and added tests to CircleCI

--- test/button_test.js
+++ test/button_test.js
@@ -1,30 +1,28 @@
-"use strict";
+/* eslint-env node, mocha */
 
-let assert = require("assert");
-let utils = require("./utils");
-let firefox = require("selenium-webdriver/firefox");
-let Context = firefox.Context;
+const assert = require("assert");
+const utils = require("./utils");
+// const firefox = require("selenium-webdriver/firefox");
+// const Context = firefox.Context;
 
 // TODO create new profile per test?
 // then we can test with a clean profile every time
 
-describe("Example Add-on Functional Tests", function () {
+describe("Example Add-on Functional Tests", function() {
   // This gives Firefox time to start, and us a bit longer during some of the tests.
   this.timeout(10000);
 
   let driver;
 
-  before(async () => {
+  before(async() => {
     const newDriver = await utils.promiseSetupDriver();
     driver = newDriver;
     return Promise.resolve();
   });
 
-  after(function() {
-    return driver.quit();
-  });
+  after(() => driver.quit());
 
-  it("should have a toolbar button", async () => {
+  it("should have a toolbar button", async() => {
     const button = await utils.promiseAddonButton(driver);
     const text = await button.getAttribute("tooltiptext");
     assert.equal(text, "Share this page");
