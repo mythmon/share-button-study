1ce63150e79e6d2957b0225350ec9c649c26b78b
Marc Robert Wong <marcrobertwong@gmail.com>
2017-07-05 13:54:47 -0700

    Modified path_webdriver.sh to work on CircleCI

--- bin/patch_webdriver.sh
+++ bin/patch_webdriver.sh
@@ -5,5 +5,6 @@
 #   - https://github.com/SeleniumHQ/selenium/commit/6907a129a3c02fe2dfc54700137e7f9aa025218a
 #   - https://github.com/mozilla/geckodriver/issues/683
 
-cd "${0%/*}"
-sed -e "2189s/.*/setParameter('text', keys.then(keys => keys.join('')))./" -i "" ../node_modules/selenium-webdriver/lib/webdriver.js
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
+sed -i -e "2189s/.*/setParameter('text', keys.then(keys => keys.join('')))./" ../node_modules/selenium-webdriver/lib/webdriver.js
