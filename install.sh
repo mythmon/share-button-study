761e5705479c2c30784cb7782477d03cd72b4b96
Marc Robert Wong <marcrobertwong@gmail.com>
2017-07-05 14:02:49 -0700

    Added xsel to dependencies

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ curl -sL https://deb.nodesource.com/setup_8.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y
-apt-get install -y zip firefox xvfb nodejs yarn
+apt-get install -y zip firefox xvfb nodejs yarn xsel
