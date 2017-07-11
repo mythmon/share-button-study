03a9cc988acabb02eab49580b2bffcd64fac276e
Marc Robert Wong <marcrobertwong@gmail.com>
2017-07-11 12:11:57 -0700

    Added comment explaining purpose of manual_test.js.

--- manual_test.js
+++ manual_test.js
@@ -1,5 +1,13 @@
 /* eslint-env node */
 
+/* This file is a helper script that will install the extension from the .xpi
+ * file and setup useful preferences for debugging. This is the same setup
+ * that the automated Selenium-Webdriver/Mocha tests run, except in this case
+ * we can manually interact with the browser.
+ * NOTE: If changes are made, they will not be reflected in the browser upon
+ * reloading, as the .xpi file has not been recreated.
+ */
+
 const utils = require("./test/utils");
 // const webdriver = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
